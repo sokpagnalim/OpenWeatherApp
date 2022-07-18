92749ccac473c5a1b09a438ac5cc95791cc6f5d5
Sokpagna Lim <sokpagnalim@gmail.com>
2022-07-18 11:44:07 -0700

    API JSON Exercise 2

---
+++ OpenWeatherApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+
+namespace OpenWeatherApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string key=File.ReadAllText("appsettings.json");
+            string APIKey = JObject.Parse(key).GetValue("APIKey").ToString();
+           Console.WriteLine("Please enter your zipcode");
+            var zipCode=Console.ReadLine();
+            var apiCall = $"https://api.openweathermap.org/data/2.5/weather?zip={zipCode},&units=imperial&appid={APIKey}";
+            Console.WriteLine();
+            Console.WriteLine($"It is currently {WeatherMap.GetTemp(apiCall)} degree F in your location.");
+
+        }
+    }
+}
